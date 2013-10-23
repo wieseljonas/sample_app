afe70ad00c6dc2f59507d3f153b59be4d76e8614
Jonas Wiesel <jonaswiesel@Jonass-MacBook-Pro.local>
2013-10-24 00:29:51 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
 				presence: true, 
 				length: { maximum: 50 }, 
 				uniqueness: { case_sensitive: false }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates 	:email, 
 				presence: true, 
 				format: { with: VALID_EMAIL_REGEX }, 
 				uniqueness: { case_sensitive: false }
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 	has_secure_password
   	validates :password, length: { minimum: 6 }
 end
